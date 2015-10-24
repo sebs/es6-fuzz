c2c74fdd0716e2a0e00dcce88cda6c1d9cfb0dfb
sebs <sebs@2xs.org>
2015-10-24 20:12:36 +0200

    added Contant type

---
+++ test/constant-test.js
@@ -0,0 +1,11 @@
+import { Constant } from '../lib/curve/constant';
+import assert from 'assert';
+
+describe('Constant', function() {
+  it('is a function', function() {
+    assert.equal(typeof Constant, 'function');
+  });
+  it('can create a new instance', function() {
+    var shape = new Constant(1);
+  });
+});
