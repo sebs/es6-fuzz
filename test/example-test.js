d298b323bdebf31d9ba03b9d696c132b41244363
Sebastian Schürmann <sebs@2xs.org>
2022-06-02 12:55:50 +0200

    test: add another example to the tests

---
+++ test/example-test.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+const Logic = require('../lib/logic');
+const Triangle = require('../lib/curve/triangle');
+
+describe('Example Test', function() {
+    var fuzzyvariable_pageSize;
+    beforeEach(()=>{
+        const logic = new Logic();
+        fuzzyvariable_pageSize = logic
+            .init('Excellent', new Triangle(20, 40, 60))
+            .or('Medium', new Triangle(40,60,80))
+            .or('Bad', new Triangle(60,80,100))
+        
+    })
+
+    it('40 is excellent', ()=>{
+        assert.equal(fuzzyvariable_pageSize.defuzzify(40).defuzzified, 'Excellent');
+    })
+    it('60 is medium', ()=>{
+        assert.equal(fuzzyvariable_pageSize.defuzzify(60).defuzzified, 'Medium');
+    })
+    it('80 is bad', ()=>{
+        assert.equal(fuzzyvariable_pageSize.defuzzify(80).defuzzified, 'Bad');
+    });
+});
