8839cb9eb6e8dafef303801b57cd019cd120b880
Sebastian Schürmann <sebastian.schuermann@maibornwolff.de>
2025-06-26 00:48:11 +0200

    docs: add a clearer test case for constant

--- test/constant-test.js
+++ test/constant-test.js
@@ -6,9 +6,10 @@ describe('Constant', function() {
   it('is a function', function() {
     assert.equal(typeof Constant, 'function');
   });
-  it('can create a new instance', function() {
+  it('same value at 5 and 100', function() {
     var shape = new Constant(1);
-    var res = shape.fuzzify(5);
-    assert.equal(res, 1);
+    var resFive = shape.fuzzify(5);
+    var resHundret = shape.fuzzify(100);
+    assert.equal(resFive, resHundret);
   });
 });
